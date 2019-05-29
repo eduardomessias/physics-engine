17563a61d745512ec7e8c413791836f55ed64a0e
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2019-05-29 15:55:13 +0200

    Create ball.js

---
+++ src/ball.js
@@ -0,0 +1,25 @@
+class Ball extends PhysicBody {
+  constructor() {
+    super();
+
+    this.color = 127;
+    this.borderColor = 255;
+    this.borderWeight = 2;
+    this.alpha = 90;
+    this.radius = 20;
+  }
+
+  display() {
+    push();
+    stroke(this.borderColor);
+    strokeWeight(this.borderWeight);
+
+    let ellipseColor = color(this.color);
+    ellipseColor.setAlpha(this.alpha);
+
+    fill(ellipseColor);
+    
+    ellipse(this.location.x, this.location.y, this.radius, this.radius);
+    pop();
+  }
+}
