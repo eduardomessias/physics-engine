3274df7f1f3fbfccd125e3bb4e7c6edd97188c19
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2019-05-29 16:42:27 +0200

    Update box.js

--- src/box.js
+++ src/box.js
@@ -8,10 +8,18 @@ class Box extends PhysicBody {
     this.alpha = 99;
     this.width = 20;
     this.height = 10;
+    this.mode = CENTER;
+    this.translate = {
+      x: width / 2,
+      y: height / 2
+    }
+    this.rotation = 0;
   }
   
   display() {
     push();
+    rectMode(this.mode);
+    
     stroke(this.borderColor);
     strokeWeight(this.borderWeight);
 
@@ -19,6 +27,9 @@ class Box extends PhysicBody {
     rectColor.setAlpha(this.alpha);
 
     fill(rectColor);
+    
+    translate(this.translate.x, this.translate.y);
+    rotate(this.rotation);
 
     rect(this.location.x, this.location.y, this.width, this.height);
     pop();
